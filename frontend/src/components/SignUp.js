538cfa33a62bf811b8146d8cae51654593e8ca77
Vashu-beep <vashu0725@gmail.com>
2025-09-13 22:00:37 +0530

    Update SignUp.js

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -20,7 +20,7 @@ const SignUp =()=>{
     
     const collectData=async ()=>{
         console.warn(name,email,password);
-        let result = await fetch('http://localhost:5000/register',{
+        let result = await fetch('https://e-comm-dash-2-0roj.onrender.com/register',{
             method: 'post',
             body: JSON.stringify({name, email, password}),
             headers:{
