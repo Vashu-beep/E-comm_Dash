ef31c4947ac31dc3b4c828238512109856f84834
vashu <vashu0725@gmail.com>
2025-09-13 15:45:58 +0530

    first commit

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,2 +1,12 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/e-commerce");
+require("dotenv").config();
+
+
+const mongoUri = process.env.MONGO_URI;
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("✅ MongoDB connected"))
+.catch(err => console.error("❌ MongoDB connection error:", err));
