20be9b9ed5fb8d35684a872c8ae6258f1501782e
Vashu-beep <vashu0725@gmail.com>
2025-09-13 21:59:49 +0530

    Update ProductList.js

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -9,7 +9,7 @@ const [products, setProducts] = useState([]);
   }, []);
 
   const getProducts = async () => {
-    let result = await fetch("http://localhost:5000/products",{
+    let result = await fetch(`https://e-comm-dash-2-0roj.onrender.com/products`,{
       headers:{
         authorization: `bearer ${JSON.parse(localStorage.getItem('token'))}`
       }
@@ -19,7 +19,7 @@ const [products, setProducts] = useState([]);
   };
 
   const deleteProduct = async (id) => {
-    let result = await fetch(`http://localhost:5000/product/${id}`, {
+    let result = await fetch(`https://e-comm-dash-2-0roj.onrender.com/product/${id}`, {
       method: "Delete",
       headers:{
         authorization: `bearer ${JSON.parse(localStorage.getItem('token'))}`
@@ -35,7 +35,7 @@ const [products, setProducts] = useState([]);
     console.warn(event.target.value);
     let key = event.target.value;
     if (key) {
-      let result = await fetch(`http://localhost:5000/search/${key} `,{
+      let result = await fetch(`https://e-comm-dash-2-0roj.onrender.com/search/${key} `,{
         headers:{
         authorization: `bearer ${JSON.parse(localStorage.getItem('token'))}`
       }
